2d6f875664ea4596b830b754ddeb24260f445abb
rachelswart <rachel.swart@cabinetoffice.gov.uk>
2024-01-17 10:19:07 +0000

    added more line breaks

--- src/main/java/gov/cabinetoffice/gap/service/OdtService.java
+++ src/main/java/gov/cabinetoffice/gap/service/OdtService.java
@@ -142,7 +142,7 @@ public class OdtService {
                                     responseParagraph.addContentWhitespace(String.join(",\n",
                                             question.getMultiResponse()) + "\n");
                                 } else {
-                                    responseParagraph.addContentWhitespace("Not provided");
+                                    responseParagraph.addContentWhitespace("Not provided" +  "\n");
                                 }
                                 break;
                             case SingleFileUpload:
@@ -152,7 +152,7 @@ public class OdtService {
                                     responseParagraph.addContentWhitespace("File name: " + question.getResponse().substring(0, index) + "\n");
                                     responseParagraph.addContentWhitespace("File extension: " + question.getResponse().substring(index + 1) + "\n");
                                 } else {
-                                    responseParagraph.addContentWhitespace("Not provided");
+                                    responseParagraph.addContentWhitespace("Not provided" +  "\n");
                                 }
                                 break;
                             case Date:
